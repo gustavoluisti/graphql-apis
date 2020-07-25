b256ae2cfbb4f560d56dbe19bf20618cf8ac245b
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-07-25 17:52:15 -0300

    Initial Commit

---
+++ banco-de-dados/projeto/knexfile.js
@@ -0,0 +1,12 @@
+const { connection } = require("./.env");
+module.exports = {
+  client: "mysql",
+  connection,
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+  },
+};
